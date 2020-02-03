ca63c189d4584948b1611e0a9b42dd855864f106
yospig <hotaquariusplus@gmail.com>
2020-02-03 12:13:11 +0900

    Add: FirebaseAuth logout

--- app/src/main/java/com/yospig/checkinignitor/CheckinListActivity.kt
+++ app/src/main/java/com/yospig/checkinignitor/CheckinListActivity.kt
@@ -1,7 +1,11 @@
 package com.yospig.checkinignitor
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -23,12 +27,40 @@ class CheckinListActivity : AppCompatActivity() {
         }
     }
 
+    // view menu
+    override fun onCreateOptionsMenu(menu: Menu?):Boolean{
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.logout -> {
+                FirebaseAuth.getInstance().signOut()
+                Log.d(TAG, "Logout.")
+                Toast.makeText(
+                    this,
+                    "Logout.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                val intent = Intent(this,MainActivity::class.java).apply{}
+                startActivity(intent)
+                return true
+            }
+            else -> {return super.onOptionsItemSelected(item)}
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         getCurrentUser()
     }
 
-    fun getCurrentUser() {
+
+
+    private fun getCurrentUser() {
         val user = FirebaseAuth.getInstance().currentUser
         user?.let{
             val name = user.displayName
