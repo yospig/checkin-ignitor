618354b6c4b5349c07de2fedda78fa1a2a58d516
yospig <hotaquariusplus@gmail.com>
2020-02-07 14:50:30 +0900

    Add: update profile by providerData

--- app/src/main/java/com/yospig/checkinignitor/CheckinListActivity.kt
+++ app/src/main/java/com/yospig/checkinignitor/CheckinListActivity.kt
@@ -6,15 +6,21 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Source
 import kotlinx.android.synthetic.main.activity_checkin_list.*
 
 class CheckinListActivity : AppCompatActivity() {
 
     private val TAG = "CheckinListActivity"
+    // Access a Cloud Firestore instance from your Activity
+    val db = FirebaseFirestore.getInstance()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,21 +64,53 @@ class CheckinListActivity : AppCompatActivity() {
         getCurrentUser()
     }
 
-
-
-    private fun getCurrentUser() {
+    private fun getCurrentUser(): FirebaseUser? {
         val user = FirebaseAuth.getInstance().currentUser
         user?.let{
-            val name = user.displayName
-            val email = user.email
-            val photoUrl = user.photoUrl
-            val emailVerified = user.isEmailVerified
-            val uid = user.uid
+            var name = it.displayName
+            var photoUrl = it.photoUrl
+            val email = it.email
+            val emailVerified = it.isEmailVerified
+            val uid = it.uid
+            for(userInfo in user.providerData){
+                if (name.isNullOrEmpty() && userInfo.displayName != null){
+                    name = userInfo.displayName
+                }
+                if (photoUrl == null && userInfo.photoUrl != null){
+                    photoUrl = userInfo.photoUrl
+                }
+            }
+            val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(name).setPhotoUri(photoUrl).build()
+            updateProfile(user, profileUpdates)
             Log.d(TAG, "name:$name")
             Log.d(TAG, "email:$email")
             Log.d(TAG, "photoUrl:$photoUrl")
             Log.d(TAG, "emailVerified:$emailVerified")
             Log.d(TAG, "uid:$uid")
+        }
+        return user
+    }
+
+    private fun updateProfile(user: FirebaseUser, profileUpdates: UserProfileChangeRequest) {
+        user.updateProfile(profileUpdates)?.addOnCompleteListener{ task ->
+            if(task.isSuccessful){
+                Log.d(TAG, "User profile updated.")
+            }
+        }
+    }
+
+    private fun fetchOwnCheckinList(user: String){
+        val docRef = db.collection("attendance_user").document(user)
+        val source = Source.CACHE
+
+        docRef.get(source).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                // offline cache
+                val document = task.result
+                Log.d(TAG, "Cached document data: ${document?.data}")
+           }else{
+                Log.d(TAG, "Cached get failed: ", task.exception)
+            }
         }
     }
 }
