a1b179809c680f5de6f8e252768e6565900f04d8
yospig <hotaquariusplus@gmail.com>
2020-03-16 11:49:42 +0900

    Add TimePicker

--- app/src/main/java/com/yospig/checkinignitor/CheckInOutActivity.kt
+++ app/src/main/java/com/yospig/checkinignitor/CheckInOutActivity.kt
@@ -1,14 +1,16 @@
 package com.yospig.checkinignitor
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.fragment.app.FragmentActivity
 import kotlinx.android.synthetic.main.activity_check_inout.*
 import java.util.*
 
-class CheckInOutActivity: FragmentActivity(),DatePickerDialog.OnDateSetListener {
+class CheckInOutActivity: FragmentActivity(),DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -16,12 +18,22 @@ class CheckInOutActivity: FragmentActivity(),DatePickerDialog.OnDateSetListener 
     }
 
     override fun onDateSet(view: DatePicker, year: Int, month: Int, dayOfMonth: Int) {
-        val yyyymmddStr = String.format(Locale.US, "%d/%d/%d", year,month+1,dayOfMonth)
-        dateView.text = yyyymmddStr
+        val dateStr = String.format(Locale.US, "%d/%d/%d", year,month+1,dayOfMonth)
+        dateView.text = dateStr
     }
 
     fun showDatePickerDialog(v: View){
         val newFragment = DatePick()
         newFragment.show(supportFragmentManager,"datePicker")
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        val timeStr = String.format(Locale.US, "%d:%d", hourOfDay,minute)
+        timeView.text = timeStr
+    }
+
+    fun showTimePickerDialog(v: View) {
+        val newFragment = TimePick()
+        newFragment.show(supportFragmentManager, "timePicker")
     }
 }
--- app/src/main/java/com/yospig/checkinignitor/TimePick.kt
+++ app/src/main/java/com/yospig/checkinignitor/TimePick.kt
@@ -1,4 +1,29 @@
 package com.yospig.checkinignitor
 
-class TimePick {
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class TimePick : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        return TimePickerDialog(
+            activity, activity as CheckInOutActivity?,
+            hour,
+            minute,
+            true
+        )
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
